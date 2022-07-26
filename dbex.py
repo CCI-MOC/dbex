0f27831ad52243ba55d2160eea338a70e29368bc
rob-baron <robbaron@bu.edu>
2022-07-25 20:45:31 -0400

    adding a larger amount of data to the longblob to tax the database a bit more

--- dbex.py
+++ dbex.py
@@ -12,11 +12,7 @@ import mysql.connector
 
 def exec_sql(cursor, sql_stmt, params, error_msg):
     """executes a simple sql statement"""
-    try:
-        cursor.execute(sql_stmt, params)
-    except Exception as err:
-        print(f"{str(err)} \n {error_msg}")
-        sys.exit()
+    cursor.execute(sql_stmt, params)
 
 
 def exec_fetchall(cursor, sql_stmt, params, error_msg):
@@ -40,8 +36,7 @@ def exec_fetchone(cursor, sql_stmt, params, error_msg):
         sys.exit()
     if result:
         return result[0]
-    else:
-        return None
+    return None
 
 
 def create_file_share_db(cursor):
@@ -54,34 +49,29 @@ def create_file_share_db(cursor):
         exec_sql(cursor, "create database file_share_db default character set 'utf8'", None, "Unable to create database")
         exec_sql(
             cursor,
-            "create table file_share_db.file ( script varchar(500), file_name varchar(2000), file_data blob, primary key (script))",
+            "create table file_share_db.file ( script varchar(500), file_name varchar(2000), file_data longblob, primary key (script))",
             None,
             "Unable to create table",
         )
 
 
-def write_data_to_db(cursor, script):
+def write_data_to_db(cursor, data, script):
     """write some data to the database"""
-    text_data = "The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog.  The quick brown fox jumped over the lazy dog."
-
     count = exec_fetchone(
-        cursor,
-        "select count(*) from file_share_db.file where script=%s",
-        (script,),
-        f"Unable to get table count from file_share_db.file.script={script}",
+        cursor, "select count(*) from file_share_db.file where script=%s", (script,), f"Unable to get table count from file_share_db.file.script={script}"
     )
     if count == 0:
         exec_sql(
             cursor,
             "insert into file_share_db.file (script, file_name, file_data) values (%s,%s,%s)",
-            (script, "f.name", text_data),
+            (script, "f.name", data),
             "Unable to insert file to db",
         )
     else:
         exec_sql(
             cursor,
             "update file_share_db.file set file_name=%s, file_data=%s where script=%s",
-            ("f.name", text_data, script),
+            ("f.name", data, script),
             "Unable to update file to db",
         )
 
@@ -103,18 +93,28 @@ def get_data_from_db(cnx, script):
     return data
 
 
+def gen_data(prev, x):
+    kernel = "i+dmOkbKAvoJAPfDSwiMurkeo6S6h7ezv943hWs0KqjBHUvly2jr708y1TrlSS9X3h8HRVSn6pWT"
+    data = prev.join(kernel for i in range(4780))
+    return data
+
+
 def main():
     cnx = mysql.connector.connect(host="mariadb", user="root", password="pass")
     cursor = cnx.cursor()
     create_file_share_db(cursor)
-    write_data_to_db(cursor, "test1")
     cnx.commit()
     cnx.close()
-
+    cnt = 1
+    data = ""
     while 1:
+        print(f"count = {cnt}")
+        data = gen_data(data, cnt)
         cnx = mysql.connector.connect(host="mariadb", user="root", password="pass")
+        write_data_to_db(cnx.cursor(), data, "test" + str(cnt))
         get_data_from_db(cnx, "test1")
         cnx.close()
+        cnt = cnt + 10
 
 
 main()
